1fc6f87839aba643eecfbf453bcb7ea643e96eca
RadekKosiada <kosiadar@gmail.com>
2025-07-17 12:56:34 +0200

    add onChange + handleSubmit to client's Registration

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,16 +1,39 @@
+"use client";
+
+import axios from "axios";
+import { FormEvent, useState } from "react";
+
 export default function Registration() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (event: FormEvent) => {
+        event.preventDefault();
+        const response = await axios.post('/api/register', { email, password });
+    };
 
     return (
         <>
             <h1>Register</h1>
-            <form>
+            <form onSubmit={(event: FormEvent) => handleSubmit(event)}>
                 <div>
                     <label>E-mail</label>
-                    <input type="email" placeholder="Your email" name="email" required></input>
+                    <input
+                        type="email"
+                        placeholder="Your email"
+                        name="email" required
+                        onChange={(event) => setEmail(event.target.value)}
+                    ></input>
                 </div>
                 <div>
                     <label>Password</label>
-                    <input type="password" placeholder="Your password" name="password" minLength={8} required></input>
+                    <input
+                        type="password"
+                        placeholder="Your password"
+                        name="password"
+                        minLength={8} required
+                        onChange={(event) => setPassword(event.target.value)}
+                    ></input>
                 </div>
                 <input type="submit" value="Register" />
             </form>
