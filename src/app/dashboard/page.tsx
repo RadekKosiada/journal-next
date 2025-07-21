473d390d64f04d0034c566127ab8889fa0b28f0b
Radoslaw Kosiada <kosiada@int.skygate.de>
2025-07-21 15:21:04 +0200

    add Links to Nav

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Dashboard() {
     return (
       <div>
         <h1>Dashboard</h1>
+        <Link href="/new-note">Create a new note</Link>
 
       </div>
     );
