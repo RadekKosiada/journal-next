2796540441cddc8bdf698e059356efd196ddcfab
Radoslaw Kosiada <kosiada@int.skygate.de>
2025-07-21 14:14:54 +0200

    hide Registration + add H1 to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import Registration from "./register/page";
+// import Registration from "./register/page";
 
 export default function Home() {
   return (
     <div>
-      <Registration />
-
+      {/* <Registration /> */}
+      <h1>Home Page</h1>
+      
     </div>
   );
 }
